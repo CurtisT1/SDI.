32ca816a0ffced97b5f1815f950a70307596e53c
CurtisT1 <curtist@fullsail.edu>
2014-04-17 20:18:11 -0500

    Update Tate_Curtis_Project3.js
    added other boolean

--- Project3/Tate_Curtis_Project3.js
+++ Project3/Tate_Curtis_Project3.js
@@ -33,4 +33,54 @@ console.log("",character1, "choose "weapon2,"for more powerful magic. ",characte
 console.log("Then",character1,"had a choice to conjure" ,move1," or ",move2,".");
 
 
+// Boolean
+
+
+var Use staff gives more power = true;
+
+if (Use staff gives more power === true); {
+	console.log (" character1 beat character2 with one move.");
+};
+
+
+FightingaGiant = Confirm ("How Are We going to Destroy the blue giant")
+
+// Prompt
+
+if (fightingAgiant === true){
+    Myprompt = prompt("how many blue giants are we going to fight?", "type number of giants");
+	Myprompt = parseInt("1");
+	if (Myprompt <= 1)
+	alert("lets destroy the one blue giant with the wizard staff");
+	else
+	alert("use wand and try to destroy the blue giant slowly");    
+    
+    
+//Function
+    
+    var  chooseWeapon = function (character1Name, weapon1) {
+		console.log("choose " + weapon1 + " character1 " + character1Name);
+	var useWeapon = function(character1Name) {
+	var weapon1 = "wand";
+		console.log("choose " + weapon1 + character1Name);
+	};	 
+	useWeapon("wizard");
+	useWeapon("blueGiant");
+	console.log(character1Name + " choose " + weapon1);
+}; 
+
+useWeapon("blueGiant", "staff");
+
+//Loop 
+
+var move2 = 2;
+while(move2 > 0) {
+	console.log(move2 + " conjure to tidal waves.");
+	move2--;
+};
+
+console.log("the blue giant is destroy.");
+
+
+
 
