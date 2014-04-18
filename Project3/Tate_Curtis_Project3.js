b9115a1bb8749474f323e4db0d1bd99ed60978b9
CurtisT1 <curtist2fullsail.edu>
2014-04-17 19:46:05 -0500

    added project3.

---
+++ Project3/Tate_Curtis_Project3.js
@@ -0,0 +1,36 @@
+//alert("JavaScript works!");
+
+//Curtis Tate
+//SDI 1404
+//Project 3 
+
+
+// My variables 
+
+
+var character1 = "Wizard";
+var character2 = "blue giant";
+var blueroad = "short way";
+var yellowroad = "long way";
+var weapon1 = "wand";
+var weapon2 = "staff";
+var move1 = "tsunami";
+var move2 = "tidal wave";
+var NumberofGiants = 1;
+var FightingAgiant = true;
+var Myprompt;
+
+
+// output of my variables  
+
+
+console.log("While on his quest" ,character1, "came across too roads" a ,blue road, "that was the short way to the giant.");
+console.log("a",yellow road,"which was the long way to the giant.");
+console.log(" ",character1, "took the" ,blueroad, "When he arrives at the" ,character2"); 
+console.log(" ",character2, "conjures a firer storm at him. The" ,character1, "quickly dodges it and had to quickly choose" ,weapon1," "or" "weapon2.".");
+console.log("",Weapon1," is faster and gives the" ,character1, "more movement. ",weapon2," is heavy and drains a half of his magic energy.");
+console.log("",character1, "choose "weapon2,"for more powerful magic. ",character2, "hurls the biggest fireball at" ,weapon2, "and he conjures a waterfall and extinguishes the fire.");
+console.log("Then",character1,"had a choice to conjure" ,move1," or ",move2,".");
+
+
+
